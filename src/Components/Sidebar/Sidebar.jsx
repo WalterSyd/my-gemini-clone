6c53326d3ba14a88fc30f2d9ce392cbfbc2185ff
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-08-20 10:42:12 +0300

    initial commit

---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "./Sidebar.css";
+import { assets } from "../../assets/assets";
+const Sidebar = () => {
+  //[1] creating functionality to extend/collapse the sidebar
+  const [extended, setExtended] = useState(false);
+  return (
+    //[0] setting up outlay of sidebar first & styling
+    <div className="sidebar">
+      <div className="top">
+        {/*implement functionality to extend/collapse the sidebar by onClickevent listener & menu icon*/}
+        <img onClick={() => setExtended(prev=>!prev)} className="menu" src={assets.menu_icon} alt="" />
+        <div className="new-chat">
+          <img src={assets.plus_icon} alt="" />
+          {extended ? <p>New Chat</p> : null}{" "}
+          {/*ternary operator to show/hide contents based on state*/}
+        </div>
+        <br />
+        {/*ternary operator to show/hide contents based on state*/}
+        {extended ? (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            <div className="recent-entry">
+              <img src={assets.message_icon} alt="" />
+              <p>What is react ...</p>
+            </div>
+          </div>
+        ) : null}
+      </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="" />
+          {extended ? <p>Help</p> : null}
+        </div>
+        <br />
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="" />
+          {extended ? <p>Activity</p> : null}
+        </div>
+        <br />
+        <div className="bottom-item recent-entry">
+          <img src={assets.setting_icon} alt="" />
+          {extended ? <p>Settings</p> : null}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
